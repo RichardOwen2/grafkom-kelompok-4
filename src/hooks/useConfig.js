ca5a0c3531ee8d82f362cf93d36e0c3d7fda5945
unknown <richardowen2411@gmail.com>
2023-02-27 00:01:27 +0800

    fixed all things

--- src/hooks/useConfig.js
+++ src/hooks/useConfig.js
@@ -4,8 +4,12 @@ export default function useConfig({ rumusJarakBayangan, rumusUkuranBayangan }) {
   const [ukuranBenda, setUkuranBenda] = useState(50);
   const [jarakBenda, setJarakBenda] = useState(200);
   const [titikFokus, setTitikFokus] = useState(-100);
-  const [jarakBayangan, setJarakBayangan] = useState(null);
-  const [ukuranBayangan, setukuranBayangan] = useState(null);
+
+  const jarakBayanganBaru = rumusJarakBayangan(titikFokus, jarakBenda);
+  const ukuranBayanganBaru = rumusUkuranBayangan(jarakBayanganBaru, ukuranBenda, jarakBenda);
+
+  const [jarakBayangan, setJarakBayangan] = useState(jarakBayanganBaru);
+  const [ukuranBayangan, setukuranBayangan] = useState(ukuranBayanganBaru);
 
   useEffect(() => {
     const jarakBayanganBaru = rumusJarakBayangan(titikFokus, jarakBenda);
