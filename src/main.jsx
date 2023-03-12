1bf48b04dc74d765d59106c61477151a1a6dd99b
unknown <richardowen2411@gmail.com>
2023-03-12 18:52:28 +0800

    refractoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import * as te from 'tw-elements';
 import './index.css';
 import App from './App';
 import 'flowbite';
