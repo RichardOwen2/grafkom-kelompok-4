b149cb946718fbc0072eedd1a126dc28a77467d7
ArchieDylan <archie3036@gmail.com>
2023-03-12 18:33:56 +0800

    view

--- src/pages/CerminCekung.jsx
+++ src/pages/CerminCekung.jsx
@@ -156,38 +156,43 @@ export default function CerminCekung() {
 
 	return (
 		<div className="p-20">
-			<div>
-				<label htmlFor="ukuranBenda">Ukuran Benda</label>
-				<input
-					id="ukuranBenda"
-					name="ukuranBenda"
-					type="number"
-					onChange={onUkuranBendaChange}
-					value={ukuranBenda}
-				/>
+			<div className="grid grid-cols-1 gap-4 bg-red-500 w-3/6 align-right">
+				<div>
+					<label htmlFor="ukuranBenda">Ukuran Benda</label>
+					<input
+						id="ukuranBenda"
+						name="ukuranBenda"
+						type="number"
+						onChange={onUkuranBendaChange}
+						value={ukuranBenda}
+					/>
+					<input id="medium-range" type="range" value="50" class="w-full h-2 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
+				</div>
+				<div>
+					<label htmlFor="ukuranBenda">Jarak Benda</label>
+					<input
+						id="ukuranBenda"
+						name="ukuranBenda"
+						type="number"
+						onChange={onJarakBendaChange}
+						value={jarakBenda}
+					/>
+					<input id="medium-range" type="range" value="50" class="w-full h-2 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
+				</div>
+				<div>
+					<label htmlFor="ukuranBenda">Titik Fokus Benda</label>
+					<input
+						id="ukuranBenda"
+						name="ukuranBenda"
+						type="number"
+						onChange={onTitikFokusChange}
+						value={titikFokus}
+					/>
+					<input id="medium-range" type="range" value="20" class="w-full h-2 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
+				</div>
 			</div>
 
-			<div>
-				<label htmlFor="ukuranBenda">Jarak Benda</label>
-				<input
-					id="ukuranBenda"
-					name="ukuranBenda"
-					type="number"
-					onChange={onJarakBendaChange}
-					value={jarakBenda}
-				/>
-			</div>
-
-			<div>
-				<label htmlFor="ukuranBenda">Titik Fokus Benda</label>
-				<input
-					id="ukuranBenda"
-					name="ukuranBenda"
-					type="number"
-					onChange={onTitikFokusChange}
-					value={titikFokus}
-				/>
-			</div>
+			
 
 			<Canvas initialDraw={initialDraw} width={canvasWidth} height={canvasHeight} />
 		</div>
