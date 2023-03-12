461a74d11afc0da39f3abadb18935e25f64ab7f4
unknown <richardowen2411@gmail.com>
2023-03-12 20:52:00 +0800

    adding slider

---
+++ src/components/SideBarInput.jsx
@@ -0,0 +1,40 @@
+export default function SideBarInput({ children }) {
+  return (
+    <div
+      className="invisible fixed bottom-0 top-0 right-0 z-[1045] flex w-96 max-w-full translate-x-full flex-col border-none bg-white bg-clip-padding text-neutral-700 shadow-lg outline-none transition duration-300 ease-in-out [&[data-te-offcanvas-show]]:transform-none"
+      tabindex="-1"
+      id="offcanvasRight"
+      aria-labelledby="offcanvasRightLabel"
+      data-te-offcanvas-init>
+      <div className="flex items-center justify-between p-4">
+        <h5
+          className="mb-0 font-semibold leading-normal"
+          id="offcanvasRightLabel">
+          SETTING
+        </h5>
+        <button
+          type="button"
+          className="box-content rounded-none border-none opacity-50 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+          data-te-offcanvas-dismiss>
+          <span
+            className="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </span>
+        </button>
+      </div>
+
+      {children}
+    </div>
+  )
+}
